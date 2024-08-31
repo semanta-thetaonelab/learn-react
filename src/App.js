f7a945f22924847c37a88132e9cca9cf9372ddf8
sayan-majumder-github <sayanmajumder775@gmail.com>
2024-08-31 20:43:34 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+
+
+const App = () => {
+  var firstName = "Sayan";
+  let lastName = "Majumder";
+  const roll = 21;
+  const names = ["arijit","sunil",8]; //array
+  
+  const student = { //Object and scoping (as variable)
+    firstName,
+    lastName:"Singh",
+    roll:201,
+    parents:{ //Object inside Object named "parents"
+      motherName:"Anjali",
+      fatherName:"Rocky",
+      relatives:{
+        vagni:"antara",
+        vagna:"dholu",
+        vagna2nd:"bholu"
+      }
+    }
+  }
+
+  const students=[student] //object inside array
+  
+  const show = () => { //scoping (as function)
+    let lastName = "mana";
+    console.log(lastName);
+  }
+  // show();
+
+  const printInfo = () => {
+    console.log(students);
+    return `${students}`
+  }
+
+  return (
+    <>
+      <div>
+        {printInfo()}
+     
+      </div>
+    </>
+  )
+}
+
+export default App;
